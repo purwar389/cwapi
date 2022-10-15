022cf4c1842278432047b18ef7e3099cb680172e
purwar389 <purwar389@gmail.com>
2022-10-16 04:07:29 +1100

    ApiController

--- src/main/java/com/gaurav/purwar/CwRestApiApplication.java
+++ src/main/java/com/gaurav/purwar/CwRestApiApplication.java
@@ -1,5 +1,6 @@
 package com.gaurav.purwar;
 
+import com.gaurav.purwar.controller.ApiControllers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,7 +11,8 @@ public class CwRestApiApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CwRestApiApplication.class);
+		//return application.sources(CwRestApiApplication.class);
+		return application.sources(ApiControllers.class);
 	}
 
 	public static void main(String[] args) {
