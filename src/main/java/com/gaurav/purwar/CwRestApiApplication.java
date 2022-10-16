8b0de32504b9fa278237ebcbde8908d000cb9937
purwar389 <purwar389@gmail.com>
2022-10-16 21:53:43 +1100

    changed to /message

--- src/main/java/com/gaurav/purwar/CwRestApiApplication.java
+++ src/main/java/com/gaurav/purwar/CwRestApiApplication.java
@@ -5,14 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CwRestApiApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		//return application.sources(CwRestApiApplication.class);
 		return application.sources(ApiControllers.class);
+	}
+
+	@GetMapping("/message")
+	public String message(){
+		return "I am ok! how are you?";
 	}
 
 	public static void main(String[] args) {
